154a0e7d6a7844a2f235d47989170cc4ea1aebf5
Jaqueline Broccolo <jaqueline.brsilva@gmail.com>
2021-11-13 21:45:11 -0300

    feat: api get

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
 fun main(){
-    println("Olá Deevas!!!")
-    println("Preparadas para construirmos uma API?")
+
+    val userDao = UserDao()
+
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace()}
+        error(404) { ctx -> ctx.json("NOT FOUND")}
+    }.start(7000)
+
+    app.routes {
+
+        // MOSTRANDO TODOS OS USUÁRIOS
+        get("/users") { ctx ->
+            ctx.json(userDao.users)
+        }
+
+        // BUSCANDO O USUÁRIO PELO id
+        get("/users/{user-id}") { ctx ->
+            ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+        }
+    }
 }
